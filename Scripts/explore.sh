fdb8acd368193869d7ea0fd09729bb9f5724d1ad
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2021-05-07 20:35:54 -0300

    github.sh renamed to explore.sh, and moved to Scripts/ folder

---
+++ Scripts/explore.sh
@@ -0,0 +1,24 @@
+# !/bin/bash
+
+# Catching Github's explore
+curl 'https://github.com/explore' > explore
+
+# Picking relative link
+links=$(cat explore | grep 'class="text-bold"' | cut -d '=' -f5 | cut -d'"' -f2)
+
+i=1
+
+for link in $links;do
+    # User/Project
+    desc=$(cat explore | grep '<p class="color-text-secondary mb-0"><div>' | cut -d'>' -f3 | cut -d'<' -f1|cut -d$'\n' -f$i)
+    echo $(echo $link | cut -d'/' -f2-3)
+    # Href
+    echo '    https://github.com/explore'$link
+    echo '    >' $desc
+    echo
+    i=$((i+1))
+done
+
+cd ..
+
+exit

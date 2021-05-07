ebd260082f7dc2600d6573e527714b4ba6254ff8
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2021-05-07 18:59:14 -0300

    explore: added description

--- github.sh
+++ github.sh
@@ -6,6 +6,15 @@ curl 'https://github.com/explore' > scrapping
 # Picking relative link
 links=$(cat scrapping | grep 'class="text-bold"' | cut -d '=' -f5 | cut -d'"' -f2)
 
+i=1
+
 for link in $links;do
-    echo 'https://github.com/explore'$link
+    # User/Project
+    desc=$(cat scrapping | grep '<p class="color-text-secondary mb-0"><div>' | cut -d'>' -f3 | cut -d'<' -f1|cut -d$'\n' -f$i)
+    echo $(echo $link | cut -d'/' -f2-3)
+    # Href
+    echo '    https://github.com/explore'$link
+    echo '    >' $desc
+    echo
+    i=$((i+1))
 done

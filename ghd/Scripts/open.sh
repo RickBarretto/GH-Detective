3958540b58931944f6f68f045aeb1828868d94c7
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2021-05-23 20:06:55 -0300

    Fix: showing "open" on search parameters on display

--- ghd/Scripts/open.sh
+++ ghd/Scripts/open.sh
@@ -2,7 +2,7 @@
 # https://github.com/RickBarretto/GH-Detective
 
 echo
-echo 'opening links with: [' $* ']'
+echo 'opening links with: [' $2, $3, $4, $5, $6, $7, $8, $9 ']'
 for arg in $*; do
     links=$(cat toopen | grep $arg | cut -d' ' -f2)
     for link in $links; do
